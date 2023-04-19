84ec3f74f7dee5fc2f824e661fdf8fa1df2e19bb
Manuel Waechtershaeuser <m.waechtershaeuser@web.de>
2023-04-19 14:24:22 +0200

    SImulation with different output speeds

--- aufgabe1.jl
+++ aufgabe1.jl
@@ -16,15 +16,33 @@ mutable struct Container
     area::Float64
     outputDistance::Float64
     nOutputs::Int
+    outputHeight::Vector{Float64}
+    outputArea::Vector{Float64}
     waterLevel::Float64
 
-    Container(l,w,h,outputDistance) = new(
-        l,w,h,
-        l*w,
-        outputDistance,
-        floor(Int, h / outputDistance) + 1,
-        0.0 
-    )
+
+    function Container(l,w,h,outputDistance::Float64) 
+
+        ## Calculate the Area of the outputs
+
+        # 5.32 mm^2 ist die flaeche der loecher
+        targetSpeed = literPerMin_to_m3PerS(1.0)
+        waterColumn = 0.5 # m
+        outputArea = targetSpeed / sqrt(waterColumn * g * 2) # m
+
+        n = floor(Int, h / outputDistance) + 1
+        oh = vec([i * outputDistance for i in 0:n-1])
+        oa = vec([outputArea for i in 1:n])
+        new(
+            l,w,h,
+            l*w,
+            outputDistance,
+            n,
+            oh,
+            oa,
+            0.0 
+        )
+    end
 end
 
 function nActiveOutputs(c::Container)
@@ -42,47 +60,53 @@ function decreaseWaterLevel!(c::Container, ϕ::Float64, t::Float64)
     c.waterLevel = h > 0 ? h : 0
 end
 
-## Calculate the Area of the outputs
+function getOutputSpeed(c::Container)
+    ϕ = 0.0;
+    for i in 1:c.nOutputs
+        waterCol = c.waterLevel - c.outputHeight[i]
 
-# 5.32 mm^2 ist die flaeche der loecher
-targetSpeed = literPerMin_to_m3PerS(1.0)
-waterColumn = 0.5 # m
-outputArea = targetSpeed / sqrt(waterColumn * g * 2) # m
+        speed = waterCol > 0.0 ? c.outputArea[i] * sqrt(waterCol * g * 2) : 0.0
 
-## Configuration
-
-ϕ_in = literPerMin_to_m3PerS(10)
-ϕ_out = literPerMin_to_m3PerS(1)
-c = Container(0.1,0.1,1.0,0.1)
-timestep = 0.001;
+        ϕ = ϕ + speed
+    end
+    ϕ
+end
 
 ## Simulation
+function simulate(c::Container, ϕ_in::Float64, timestep::Float64)
+    time = 0
+    while true
+        
+        increaseWaterLevel!(c, ϕ_in, timestep)
 
-time = 0
-while true
-    time = time + 1;
+        if(c.waterLevel >= 0.9) 
+            break; 
+        end
+        ϕ_out = getOutputSpeed(c)
+        
+        decreaseWaterLevel!(c, ϕ_out, timestep)
 
-    increaseWaterLevel!(c, ϕ_in, timestep)
-
-    if(time % 30000 == 0) 
-        level = c.waterLevel
-        t = time * timestep
-        @show t level
+        time = time + 1;
+        if(time % 30000 == 0) 
+            level = c.waterLevel
+            t = time * timestep
+            @show t level
+        end
+        
     end
 
-    if(c.waterLevel >= 0.9) 
-        break; 
-    end
-    
-    
-    decreaseWaterLevel!(c, nActiveOutputs(c) * ϕ_out, timestep)
-    
+
+    c.waterLevel = 0
+
+    @show time * timestep
 end
 
 
-c.waterLevel = 0
 
-time * timestep
-
-
+## Configuration
+ϕ_in = literPerMin_to_m3PerS(10)
+#ϕ_out = literPerMin_to_m3PerS(1)
+c = Container(0.1,0.1,1.0,0.1)
+timestep = 0.001;
+simulate(c, ϕ_in, timestep)
 
